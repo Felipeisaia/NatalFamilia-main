ea41d97d80547ab6e09f2fa2c798356d20863daa
Felipeisaia <felipeisaia6@gmail.com>
2025-12-16 19:03:06 -0300

    dasdsadad

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://unplanted-loni-ambitionlessly.ngrok-free.dev:3000/api',
+    baseURL: ' https://unplanted-loni-ambitionlessly.ngrok-free.dev/api',
 });
 
 export default api;
