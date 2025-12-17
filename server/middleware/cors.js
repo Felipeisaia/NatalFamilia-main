85dc04ee9b6a0fc4f0bf84bf1351c57dfdb29535
Felipeisaia <felipeisaia6@gmail.com>
2025-12-17 00:21:53 -0300

    DASDSAD

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -28,7 +28,7 @@ export const corsOptions = {
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'ngrok-skip-browser-warning'],
 };
 
 export default cors(corsOptions);
--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'https://unplanted-loni-ambitionlessly.ngrok-free.dev/api',
     headers: {
+        "Content-Type": "application/json",
         "ngrok-skip-browser-warning": "true"
     }
 });
